b0d8e212534e643a8fed493578c35a8144158a71
Valdemar <valde.dk@hotmail.com>
2024-05-21 15:01:35 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+
 import About from './page/About'
+
+import Notes from './page/SomeNotes.jsx'
 
 function App() {
 
   return (
     <>
+
     
     <About/>
+
+      <Notes/>
+
     </>
   )
 }
