193a432bbf5b874a8f2b01a20d02758ce59e1414
Valdemar <valde.dk@hotmail.com>
2024-05-22 14:25:09 +0200

    New Note

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import About from './page/About'
+import AddNote from './AddNote'
 
 function App() {
 
   return (
     <>
-    
     <About/>
+    <AddNote/>
     </>
   )
 }
