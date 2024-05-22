325c58f2889ee1b650754251447a763a842c7f9c
DoctorHeyde <alexanderrasmuss@gmail.com>
2024-05-22 14:12:58 +0200

    Styled components and createuser finished
    Just missing background and some nicer colors, but this is just a nice to have.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import About from './page/About'
+import CreateUser from './features/CreateUser'
 
 function App() {
 
   return (
     <>
-    <About/>
+    {/* <About/> */}
+    <CreateUser/>
     </>
   )
 }
