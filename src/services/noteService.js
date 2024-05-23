9cfeeb6f0b92f7ee44bb760cb4d86a48095459dc
DavidAH <cph-dh223@cphbusiness.dk>
2024-05-23 10:53:34 +0200

    created fetchs

---
+++ src/services/noteService.js
@@ -0,0 +1,50 @@
+
+
+const searchByTitle = async (seachString) => {
+    try {
+        const token = localStorage.getItem("token")
+
+        const result = await fetch(`${BASE_URL_DEV}/user/notes/search/${seachString}`, {
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            }
+        });
+        
+        return await result.json();
+        
+    } catch (e) {
+        console.log(e);
+    }
+}
+const sortByCategory = async () => {
+    return sortByFetch('category')
+}
+
+const sortByTitle = async () => {
+    return sortByFetch('title')
+}
+
+const sortByDate = () => { 
+    return sortByFetch('date')
+}
+
+const sortByFetch = async (endURL) => {
+    try {
+        const token = localStorage.getItem("token")
+
+        const result = await fetch(`${BASE_URL_DEV}/user/notes/sort/${endURL}`, {
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            }
+        });
+
+        const data = await result.json();
+        return data
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+export { searchByTitle, sortByCategory, sortByTitle, sortByDate }
