d8490cbfab50c78c29e0073d767efbcf137dd6cd
DavidAH <cph-dh223@cphbusiness.dk>
2024-05-23 11:19:42 +0200

    crud created

--- src/services/noteService.js
+++ src/services/noteService.js
@@ -1,5 +1,79 @@
+const createNote = async (note) => {
+    try {
+        const token = localStorage.getItem("token")
 
+        const result = await fetch(`${BASE_URL_DEV}/user/note/create`, {
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            },
+            body: {
+                ...note
+            }
+        });
 
+        return await result.json();
+
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+const readNote = async () => {
+    try {
+        const token = localStorage.getItem("token")
+
+        const result = await fetch(`${BASE_URL_DEV}/user/note`, {
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            },
+        });
+
+        return await result.json();
+
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+const updateNote = async (note) => {
+    try {
+        const token = localStorage.getItem("token")
+
+        const result = await fetch(`${BASE_URL_DEV}/user/note/update/${note.id}`, {
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            },
+            body:{
+                ...note
+            }
+        });
+        return await result.json();
+
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+const deleteNote = async (note) => {
+    try {
+        const token = localStorage.getItem("token")
+
+        const result = await fetch(`${BASE_URL_DEV}/user/note/delete/${note.id}`, {
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            },
+        });
+
+        return await result.json();
+
+    } catch (e) {
+        console.log(e);
+    }
+}
 const searchByTitle = async (seachString) => {
     try {
         const token = localStorage.getItem("token")
@@ -10,9 +84,9 @@ const searchByTitle = async (seachString) => {
                 "Authorization": `Bearer ${token}`
             }
         });
-        
+
         return await result.json();
-        
+
     } catch (e) {
         console.log(e);
     }
@@ -25,10 +99,10 @@ const sortByTitle = () => {
     return sortByFetch('title')
 }
 
-const sortByDate = () => { 
+const sortByDate = () => {
     return sortByFetch('date')
 }
-onst sortByFetch = async (endURL) => {
+const sortByFetch = async (endURL) => {
     try {
         const token = localStorage.getItem("token")
 
