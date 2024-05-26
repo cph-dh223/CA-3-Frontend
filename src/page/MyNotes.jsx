9cd72255cf9164d54451ddad2beb44f34bb6cbf3
TRossen89 <110735176+TRossen89@users.noreply.github.com>
2024-05-26 16:34:23 +0200

    Sort and search works

--- src/page/MyNotes.jsx
+++ src/page/MyNotes.jsx
@@ -1,6 +1,12 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import { readAllNotes } from "../services/noteService";
+import {
+  readAllNotes,
+  searchByTitle,
+  sortByCategory,
+  sortByDate,
+  sortByTitle,
+} from "../services/noteService";
 
 const PageContainer = styled.div`
   display: flex;
@@ -24,7 +30,7 @@ const NoteContainer = styled.div`
 `;
 
 const NoteDiv = styled.div`
-border: solid red;
+  border: solid red;
   flex-grow: 1;
 `;
 
@@ -33,8 +39,7 @@ const EditButton = styled.button`
   color: #060606;
 `;
 
-const ActualNote = styled.div`
-
+const NoteWrapper = styled.div`
   display: flex;
   flex-direction: column;
   border: 1px solid #000000;
@@ -58,28 +63,94 @@ const StyledMainNoteDiv = styled.div`
 `;
 
 const StyledTitleDiv = styled.div`
-    border: solid purple;
+  border: solid purple;
+`;
+
+const DivForSearchBarAndSortButtons = styled.div`
+  padding: 1vw;
+  border: solid green;
+  display: flex;
+  width: 60vw;
+  margin-bottom: 4vw;
+`;
+
+const FrameDiv = styled.div`
+  border: solid orange;
+  padding: 0.5vw;
+
+  display: flex;
+  flex: 1;
+  //height: 20px;
+`;
+
+const SearchBar = styled.input`
+  width: 100%;
+`;
+
+const SortButton = styled.button`
+  flex: 1;
+`;
+
+const SortByHeadline = styled.div`
+  flex: 0.5;
+`;
+
+const GoButton = styled.button`
+
+
 `
 
 
-const Note = ({ note, title }) => {
+const Note = ({ note, title, category }) => {
   const [edit, setEdit] = useState(false);
 
   return (
     <StyledMainNoteDiv>
-    <StyledTitleDiv>{title}</StyledTitleDiv>
-      <ActualNote>
+      <StyledTitleDiv>
+        {title} - Category: {category}
+      </StyledTitleDiv>
+      <NoteWrapper>
         <EditButton onClick={() => setEdit(!edit)}>
           {edit ? "Stop Editing" : "Edit Note"}
         </EditButton>
         <NoteDiv contentEditable={edit}>{note}</NoteDiv>
-      </ActualNote>
+      </NoteWrapper>
     </StyledMainNoteDiv>
   );
 };
 
 function MyNotes() {
   const [notes, setNotes] = useState([]);
+  const [query, setQuery] = useState("");
+
+  const handleQueryChange = (e) => {
+    setQuery(e.target.value);
+  };
+
+
+  const search = async () => {
+    const allNotesFromSearch = await searchByTitle(query);
+    setNotes(allNotesFromSearch);
+
+  }
+
+  const sortNotesByCategory = async () => {
+    const allNotesSorted = await sortByCategory();
+    console.log(allNotesSorted);
+    setNotes(allNotesSorted);
+  };
+
+  const sortNotesByTitle = async () => {
+    const allNotesSorted = await sortByTitle();
+    console.log(allNotesSorted);
+    setNotes(allNotesSorted);
+  };
+
+  const sortNotesByDate = async () => {
+    const allNotesSorted = await sortByDate();
+    console.log(allNotesSorted);
+    setNotes(allNotesSorted);
+  };
 
   const fetchAllNotes = async () => {
     const allNotes = await readAllNotes();
@@ -95,10 +166,35 @@ function MyNotes() {
     <PageContainer>
       <Header>My Notes</Header>
 
-      <StyledSearchBar></StyledSearchBar>
+      <DivForSearchBarAndSortButtons>
+        <FrameDiv></FrameDiv>
+
+        <FrameDiv>
+          <SearchBar
+            type="text"
+            placeholder="Search..."
+            value={query}
+            onChange={handleQueryChange}
+          ></SearchBar>
+          <GoButton onClick={search}>Go!</GoButton>
+        </FrameDiv>
+
+        <FrameDiv>
+          <SortByHeadline>Sort by: </SortByHeadline>
+          <SortButton onClick={sortNotesByCategory}>Category</SortButton>
+          <SortButton onClick={sortNotesByTitle}>Title</SortButton>
+          <SortButton onClick={sortNotesByDate}>Date</SortButton>
+        </FrameDiv>
+      </DivForSearchBarAndSortButtons>
+
       <NoteContainer>
         {notes.map((note) => (
-          <Note key={note.id} note={note.content} title={note.title} />
+          <Note
+            key={note.id}
+            note={note.content}
+            title={note.title}
+            category={note.category}
+          />
         ))}
       </NoteContainer>
     </PageContainer>
